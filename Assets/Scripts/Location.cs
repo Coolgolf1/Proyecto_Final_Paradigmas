88e929bfb34e7069b6fdc3f4d81f90c3927b4eb7
prialgon <alpri.gonzalez@gmail.com>
2025-10-31 18:27:12 +0100

    ciudades añadidas

--- Assets/Scripts/Location.cs
+++ Assets/Scripts/Location.cs
@@ -2,6 +2,19 @@ using UnityEngine;
 
 public class Location : MonoBehaviour
 {
-    public string dd;
+    public string id;
     public Vector3 coords;
+
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        transform.position = coords;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
