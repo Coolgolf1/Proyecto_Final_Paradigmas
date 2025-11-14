8840ab3a805dbd19340bdfa5c16dca7644615326
prialgon <alpri.gonzalez@gmail.com>
2025-11-14 17:30:34 +0100

    iobject

--- Assets/Scripts/IFactory.cs
+++ Assets/Scripts/IFactory.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public interface IFactory<T> where T : Airport
+public interface IFactory<T> where T : IObject
 {
     T Build(Transform transform);
 }
--- Assets/Scripts/ITypedFactory.cs
+++ Assets/Scripts/ITypedFactory.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public interface ITypedFactory<T, TTypes> where T : Airplane
+public interface ITypedFactory<T, TTypes> where T : IObject
 {
     T Build(TTypes types, Transform transform);
 }
