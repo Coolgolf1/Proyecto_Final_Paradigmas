da216bbaa6b39ad1233b02ead800fc3bd1adbf2b
prialgon <alpri.gonzalez@gmail.com>
2026-01-10 12:58:19 +0100

    change player name and remove random import

--- Assets/Scripts/Factories/AirplaneFactory.cs
+++ Assets/Scripts/Factories/AirplaneFactory.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
 
 public abstract class AirplaneFactory : ITypedFactory<IObject, AirplaneTypes>
 {
